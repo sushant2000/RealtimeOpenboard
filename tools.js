e6f985369a498e09480825f7ca6c867a592ff1cf
sushant2000 <sushantmawar10@gmail.com>
2022-01-05 02:08:37 +0530

    update

--- tools.js
+++ tools.js
@@ -1,4 +1,21 @@
-let optionCont = document.querySelector(".options-cont");
+let optionsCont = document.querySelector(".options-cont");
+let optionsFlag = true;
 optionCont.addEventListener("click" , (e){
-    
+    optionsFlag = !optionFlag;
+
+    if(optionsFlag) openTools();
+    else closeTools();
 })
+
+function openTools(){
+    let iconsElem = optionsCont.children[0];
+    iconsElem.classList.remove("fa-times");
+    iconsElem.classList.add("fa-bars"); 
+}
+
+function closeTools(){
+    let icons = optionsCont.children[0];
+    iconsElem.classList.remove("fa-bars");
+    iconsElem.classList.add("fa-times");
+
+}
